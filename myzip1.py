641f8d248bf09081c6c717dc14472b4a994f588c
Thejazeto <aron4432@gmail.com>
2014-08-18 23:08:59 +0530

    main function introduced

--- myzip1.py
+++ myzip1.py
@@ -9,26 +9,33 @@ from zipfile import ZipFile
 import os.path
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--force", help="force action", action="store_true")
-parser.add_argument("Zipfile", help="path of the output zip file")
-parser.add_argument("Source", help="path(s) of the input files")
-args = parser.parse_args()
-
-
-def zipping():
-    with ZipFile(args.Zipfile, 'w') as myzip:
-        myzip.write(args.Source)
+# function taking in input file to zip it into output file
+def zipping(in_file, out_file):
+    with ZipFile(out_file, 'w') as myzip:
+        myzip.write(in_file)
         print("done")
 
-if args.force:
-    zipping()
-elif os.path.isfile(args.Zipfile):
-    print("The file already exists. Do you want to overwrite? y/n")
-    ans = input("=>")
-    if ans == 'y':
-        zipping()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--force", help="force action", action="store_true")
+    parser.add_argument("Zipfile", help="path of the output zip file")
+    parser.add_argument("Source", help="path(s) of the input files")
+    args = parser.parse_args()
+
+    ip = args.Source
+    op = args.Zipfile
+
+    if args.force:
+        zipping(ip, op)
+    elif os.path.isfile(op):
+        print("The file already exists. Do you want to overwrite? y/n")
+        ans = input("=>")
+        if ans == 'y':
+            zipping(ip, op)
+        else:
+            print("Try another name")
     else:
-        print("Try another name")
-else:
-    zipping()
+        zipping(ip, op)
+
+if __name__ == '__main__':
+    main()
